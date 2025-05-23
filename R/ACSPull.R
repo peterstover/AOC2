1c0c7136293688a80f7eafc6a58371692fe064ef
Peter Stover <ps8904@princeton.edu>
2025-05-23 10:08:30 -0400

    Pull function for all tracts

---
+++ R/ACSPull.R
@@ -0,0 +1,7 @@
+pull_all_tracts <- function(statistic,year,) {
+  library(tidycensus)
+  library(tidyverse)
+  library(readxl)
+  census_api_key("20157851bb2bda8e49da96a097fbdc27420a2267",install = TRUE)
+  
+}
