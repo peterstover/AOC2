ccab6d27d55aecf0ed498bc906ad2ea860700c7d
peterstover <ps8904@princeton.edu>
2025-06-02 09:48:36 -0400

    Update ACSPull.R

--- R/ACSPull.R
+++ R/ACSPull.R
@@ -2,21 +2,39 @@ library(readxl)
 library(dplyr)
 library(ggplot2)
 #import data
-  X2022_geography_reference_manual <- read_excel("C:/Users/peter/Downloads/2022-geography-reference-manual.xlsx")
+ counties<- read_excel("C:/Users/peter/Downloads/2022-geography-reference-manual.xlsx")
   RUCA <- read_excel("C:/Users/peter/Downloads/ruca2010revised (1).xlsx")
-  nonmetro_counties <- X2022_geography_reference_manual
-  #filter data
-  nonmetro_counties <- nonmetro_counties %>% filter(`GEO-TYPE`=='03')
-  nonmetro_counties <- nonmetro_counties %>% 
+
+#Filter only metro counties
+metro_msa_codes <- counties %>%
+  filter(`GEO-TYPE` =="08" & grepl("Metro Area", TITLE)) %>%
+  pull(`MSA Code`) %>%
+  unique()
+metro_counties <- counties %>%
+  filter(`GEO-TYPE` == "03", `MSA Code` %in% metro_msa_codes)
+metro_counties <- metro_counties %>% 
     mutate(FIPS_State_County = paste0(`FIPS State Code`, `FIPS County Code`))
-  nonmetro_tracts <- RUCA %>%
-    semi_join(nonmetro_counties, by = c("State-County FIPS Code" = "FIPS_State_County"))
-  metro_tracts <- RUCA %>%
-    anti_join(nonmetro_counties, by = c("State-County FIPS Code" = "FIPS_State_County"))
-  RUCA4_nm_tracts <- nonmetro_tracts %>%
-    filter(`Primary RUCA Code 2010`>=4)
-  RUCA4_m_tracts <- metro_tracts %>%
-    filter(`Primary RUCA Code 2010`>=4)
+ 
+metro_tracts <- RUCA %>%
+    semi_join(metro_counties, by = c("State-County FIPS Code" = "FIPS_State_County"))
+nonmetro_tracts <- RUCA %>%
+    anti_join(metro_counties, by = c("State-County FIPS Code" = "FIPS_State_County"))
+nmt <- nonmetro_tracts %>%
+    filter(`Primary RUCA Code 2010`==4)%>%
+   filter({
+    Q1 <- quantile(`Population Density (per square mile), 2010`, 0.25, na.rm = TRUE)
+    Q3 <- quantile(`Population Density (per square mile), 2010`, 0.75, na.rm = TRUE)
+    IQR_val <- Q3 - Q1
+    `Population Density (per square mile), 2010` >= (Q1 - 2 * IQR_val) & `Population Density (per square mile), 2010` <= (Q3 + 2 * IQR_val)
+  })
+mt<- metro_tracts %>%
+    filter(`Primary RUCA Code 2010`==4)%>%
+   filter({
+    Q1 <- quantile(`Population Density (per square mile), 2010`, 0.25, na.rm = TRUE)
+    Q3 <- quantile(`Population Density (per square mile), 2010`, 0.75, na.rm = TRUE)
+    IQR_val <- Q3 - Q1
+    `Population Density (per square mile), 2010` >= (Q1 - 2 * IQR_val) & `Population Density (per square mile), 2010` <= (Q3 + 2 * IQR_val)
+  })
 
 pull_all_tracts <- function(statistic, year) {
   library(tidycensus)
